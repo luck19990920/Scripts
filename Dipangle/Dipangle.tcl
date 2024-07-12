6ffaba0c698acb8ff70cc63f924bdf35d27d4047
luck19990920 <897320551@qq.com>
2024-07-12 16:16:45 +0800

    first commit

---
+++ Dipangle/Dipangle.tcl
@@ -0,0 +1,23 @@
+# Written by Jian Zhang (jian_zhang@cug.edu.cn)
+proc Dipangle {selIon dist_1 dist_2} {
+    set n [ molinfo top get numframes ]
+    set result [open dip.txt w]
+    for { set i 0 } { $i < $n } { incr i } {
+        set ion [ atomselect top "$selIon" frame $i ]
+        puts "[expr $i+1]/$n"
+        foreach j [$ion list] {
+            set singleion [atomselect top "index $j" frame $i]
+            set wa [ atomselect top "{not same residue as {exwithin $dist_1 of index $j }} \
+            and name OW and same residue as {exwithin $dist_2 of index $j}" frame $i]
+            foreach m [$wa get resid] {
+                set dip [measure dipole [atomselect top "resid $m" frame $i ]]
+                set ow [atomselect top "resid $m and name OW" frame $i ]
+                set vec [list [expr [$singleion get x] - [$ow get x]] \
+                [expr [$singleion get y] - [$ow get y]] [expr [$singleion get z] - [$ow get z]]]
+                puts $result "[expr [vecdot $vec $dip]/[expr [veclength $vec] * [veclength $dip]]]"
+            }
+        }
+    }
+    close $result
+}
+
---
+++ Dipangle/README.md
@@ -0,0 +1,11 @@
+`Dipangle.tcl`是使用VMD计算离子特定溶剂化层中水分子偶极矩与水分子中O与离子之间连线夹角的余弦值的脚本。
+
+### 使用方法
+先采用<a href="https://github.com/luck19990920/gmxoutchg_py" target="_blank">gmxoutchg_py</a>中所述将结构文件对应的电荷信息使VMD读入。然后，若在windows系统下，则将`Dipangle.tcl`放在VMD的安装目录下，若在Linux系统下，则将`Dipangle.tcl`放在结构文件同级目录下。随后，在VMD中输入`source Dipangle.tcl`。最后，输入以下类似的命令得到结果文件`dip.txt`：
+``` tcl
+Dipangle "resname LI" 3 5
+```
+上面的命令中，
+* `resname LI`为你所要研究的离子，应符合VMD中选择语句的要求
+* `3`为溶剂化层中水分子距离的统计下限(单位：埃)
+* `5`为溶剂化层中水分子距离的统计上限(单位：埃)
